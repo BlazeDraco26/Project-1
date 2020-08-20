5e30d1229d9f4c648aa1f5322442e0fb1f4860b4
BlazeDraco26 <jordan.cheng94303@gmail.com>
2020-08-20 09:43:28 -0700

    created

---
+++ src/Foothill.java
@@ -0,0 +1,42 @@
+
+public class Foothill
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hello World!");
+      System.out.println("My name is Jordan Cheng");
+      System.out.println("My Foothill ID is 20441267");
+      System.out.println("Syllabus Detail #1: Our instructor is");
+      System.out.println("Professor Jesse Cecil.");
+      System.out.println("Syllabus Detail #2: One should spend 20 to 26 hours");
+      System.out.println("per week working on class requirements.");
+      System.out.println("Syllabus Detail #3: Students are to participate in");
+      System.out.println("discussions on the forums where questions and");
+      System.out.println("comments will be discussed.");
+      System.out.println("Lab Requirement Detail #1: Each day late after a");
+      System.out.println("lab assignment is due will result in");
+      System.out.println("two points lost.");
+      System.out.println("Lab Requirement Detail #2: Rather than using tabs,");
+      System.out.println("use three spaces in their place.");
+   }
+}
+
+/* -------------------- run ----------------------------------
+
+Hello World!
+My name is Jordan Cheng
+My Foothill ID is 20441267
+Syllabus Detail #1: Our instructor is
+Professor Jesse Cecil.
+Syllabus Detail #2: One should spend 20 to 26 hours
+per week working on class requirements.
+Syllabus Detail #3: Students are to participate in
+discussions on the forums where questions and
+comments will be discussed.
+Lab Requirement Detail #1: Each day late after a
+lab assignment is due will result in
+two points lost.
+Lab Requirement Detail #2: Rather than using tabs,
+use three spaces in their place.
+
+----------------------------------------------------------- */
